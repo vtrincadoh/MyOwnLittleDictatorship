5e4d7a91d8e969fc2afc60910b897b564ae93d10
sebabenn1234 <65093918+sebabenn1234@users.noreply.github.com>
2020-10-12 16:49:22 -0300

    Initial
    Bases for work

---
+++ Assets/Code/C_Minister.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Minister : MonoBehaviour
+{
+    public enum alineacion
+    {
+        Economico,Humanista,Patriota
+    }
+
+    public string ministerName;
+    public alineacion myAlineacion;
+
+    public int AskVote(alineacion alineacionProyecto)
+    {
+        int desicion = 0; // 0 = rechazo y 1 = apruebo
+        if(alineacionProyecto.GetType() == myAlineacion.GetType())
+        {
+            desicion = 1;
+        }
+        else
+        {
+            desicion = Random.Range(0, 1);
+        }
+        Debug.LogFormat("Minister {0} voted {1}", ministerName, desicion);
+        return desicion;
+    }
+}
---
+++ Assets/Code/C_Proyect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Proyect : MonoBehaviour
+{
+    public enum alineacion
+    {
+        Economico, Humanista, Patriota
+    }
+
+    public string proyectName;
+}
