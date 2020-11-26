88d23d350a28fd8294ebb1ea25d0edb4eb038044
sebabenn1234 <65093918+sebabenn1234@users.noreply.github.com>
2020-11-26 18:14:33 -0300

    Feat: Minister Models added

---
+++ Assets/Code/C_MinisterHeads.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_MinisterHeads : MonoBehaviour
+{
+    public GameObject head;
+    private Material matHead,matMinister;
+
+    private void Start()
+    {
+        matMinister = this.gameObject.GetComponent<MeshRenderer>().material;
+        matHead = head.GetComponent<MeshRenderer>().material;
+    }
+
+    public void Update()
+    {
+        matHead.color = matMinister.color;
+    }
+}
