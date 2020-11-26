60e98a4e08c0cacc36992ed6a19f64a48d9d8456
sebabenn1234 <65093918+sebabenn1234@users.noreply.github.com>
2020-11-26 17:56:53 -0300

    Feat: Added Music

---
+++ Assets/Code/C_AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_AudioManager : MonoBehaviour
+{
+    AudioSource m_ASource;
+
+    private void Start()
+    {
+        m_ASource = GetComponent<AudioSource>();
+        m_ASource.Stop();
+        Invoke("playMusic", 0.5f);
+    }
+
+    void playMusic()
+    {
+        m_ASource.Play();
+    }
+}
