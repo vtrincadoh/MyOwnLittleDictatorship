0aff9c43d841407c26380bb38ab78f3addb11d83
sebabenn1234 <65093918+sebabenn1234@users.noreply.github.com>
2020-12-21 18:45:34 -0300

    Added: Universal pipeline and LightWeight RP

--- Assets/Code/C_3DRelatedButton.cs
+++ Assets/Code/C_3DRelatedButton.cs
@@ -10,6 +10,7 @@ public class C_3DRelatedButton : MonoBehaviour
 
     private void Update()
     {
+        if (this.gameObject.tag == "3DProject") AdquireColor();
         canBeInteracted = thisButton.GetComponent<Button>().interactable;
         if (thisButton.GetComponent<Image>() != null) gameObject.GetComponent<MeshRenderer>().material.color = thisButton.GetComponent<Image>().color; //Could glow
         if(thisButton.tag != "Minister")
@@ -17,6 +18,13 @@ public class C_3DRelatedButton : MonoBehaviour
             if(thisButton.interactable || thisButton.GetComponent<Image>().color != Color.black) this.gameObject.GetComponent<MeshRenderer>().enabled = true;
             else this.gameObject.GetComponent<MeshRenderer>().enabled = false;
         }
+    }
+
+    private void AdquireColor()
+    {
+        /*matMinister = this.gameObject.GetComponent<MeshRenderer>().material;
+        matHead = head.GetComponent<MeshRenderer>().material;*/
+        gameObject.GetComponent<MeshRenderer>().material.color = thisButton.GetComponent<Image>().material.color;
     }
 
     public void Selected()
