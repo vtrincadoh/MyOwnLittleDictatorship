11543caff656341a2ec16c75ff3ced0403dc2a99
sebabenn1234 <65093918+sebabenn1234@users.noreply.github.com>
2021-01-01 16:41:04 -0300

    Added: Slider progress in 3d

---
+++ Assets/Code/C_3DRelatedScrollBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class C_3DRelatedScrollBar : MonoBehaviour
+{
+    public Slider thisBar,referenceBar;
+
+    private void Start()
+    {
+        thisBar = gameObject.GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        thisBar.value = referenceBar.value;
+    }
+}
